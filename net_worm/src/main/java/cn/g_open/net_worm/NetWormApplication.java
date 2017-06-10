2ef3ac8e37be56dcc7ac7375abf1f1a017d6b911
fredgan06 <406966464@qq.com>
2017-06-10 15:36:41 +0800

    java 1233

---
+++ net_worm/src/main/java/cn/g_open/net_worm/NetWormApplication.java
@@ -0,0 +1,12 @@
+package cn.g_open.net_worm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetWormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetWormApplication.class, args);
+	}
+}
---
+++ net_worm/src/test/java/cn/g_open/net_worm/NetWormApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.g_open.net_worm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NetWormApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
