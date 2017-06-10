5a3feba2959c1e340e0a465a24691ef6718ba817
fredgan06 <406966464@qq.com>
2017-06-10 16:16:15 +0800

    测试提交

--- net_worm/src/main/java/cn/g_open/net_worm/NetWormApplication.java
+++ net_worm/src/main/java/cn/g_open/net_worm/NetWormApplication.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NetWormApplication {
+public class NetWormApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(NetWormApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(NetWormApplication.class, args);
+    }
+
+    public void aa()
+    {
+
+    }
 }
